95a918b81201b7ff7572801840cd780c51f7c13c
koumingsheng@126.com <koumingsheng@126.com>
2023-04-11 11:13:34 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:koumingsheng/koumingsheng.github.io.git main
+# git push -f git@github.com:koumingsheng/koumingsheng.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:koumingsheng/study.git main:gh-pages
 
 cd -
